ae6d6a2383922348e6347af09f5caaf6b977f605
贺雅迪 <heyadi@crc-dot1x-nat-10-239-226-89.bu.edu>
2024-03-27 12:10:27 -0400

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.photogallery
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.webkit.WebViewFragment
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+}
